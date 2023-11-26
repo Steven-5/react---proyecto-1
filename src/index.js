45f552ae02318c82c9d23c70237110b567af8ef7
SFMX <sfmx@debian.sfmx>
2023-11-25 23:00:05 -0600

    Base

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './Styles/index.css';
+import React        from 'react';
+import ReactDOM     from 'react-dom/client';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div>
+    <h1>Steven Facundo Mejía Xolop</h1>
+  </div>
+);
